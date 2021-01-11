69649b49faa6938b765264cfd84b30d2e8c39026
AshfordHastings <ashfordh1@gmail.com>
2021-01-11 17:16:39 -0600

    initial commit.

---
+++ LaborChartBot/src/main/java/labor/LaborChartBotApplication.java
@@ -0,0 +1,13 @@
+package labor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborChartBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborChartBotApplication.class, args);
+	}
+
+}
